a0cac5c35fedf5c408bc02a20f518aa974059baa
Nguyễn Thị Kim Yến <yenn0750@gmail.com>
2023-02-25 18:08:06 +0700

    create_category

--- resources/views/admin/category/create.blade.php
+++ resources/views/admin/category/create.blade.php
@@ -1,13 +1,13 @@
 @extends('adminlte::page')
 
-@section('title', 'Tambah Category')
+@section('title', 'Tambah User')
 
 @section('content_header')
-    <h1 class="m-0 text-dark">Tambah User</h1>
+    <h1 class="m-0 text-dark">Thêm mới danh mục</h1>
 @stop
 
 @section('content')
-    <form action="{{route('users.store')}}" method="post">
+    <form action="{{route('categories.store')}}" method="POST">
         @csrf
         <div class="row">
             <div class="col-12">
@@ -15,30 +15,20 @@
                     <div class="card-body">
 
                         <div class="form-group">
-                            <label for="exampleInputName">Nama</label>
-                            <input type="text" class="form-control @error('name') is-invalid @enderror" id="exampleInputName" placeholder="Nama lengkap" name="name" value="{{old('name')}}">
-                            @error('name') <span class="text-danger">{{$message}}</span> @enderror
+                          <label for="">Lựa chọn danh mục</label>
+                          <select class="form-control" name="parent_id" id="">
+                            <option value="0">Root</option>
+                          </select>
                         </div>
 
                         <div class="form-group">
-                            <label for="exampleInputEmail">Email address</label>
-                            <input type="email" class="form-control @error('email') is-invalid @enderror" id="exampleInputEmail" placeholder="Masukkan Email" name="email" value="{{old('email')}}">
-                            @error('email') <span class="text-danger">{{$message}}</span> @enderror
+                            <label for="exampleInputEmail">Tên danh mục</label>
+                            <input type="text" class="form-control @error('name') is-invalid @enderror" id="exampleInputEmail"  name="name" value="{{old('name')}}">
                         </div>
-                        <div class="form-group">
-                            <label for="exampleInputPassword">Password</label>
-                            <input type="password" class="form-control @error('password') is-invalid @enderror" id="exampleInputPassword" placeholder="Password" name="password">
-                            @error('password') <span class="text-danger">{{$message}}</span> @enderror
-                        </div>
-                        <div class="form-group">
-                            <label for="exampleInputPassword">Konfirmasi Password</label>
-                            <input type="password" class="form-control" id="exampleInputPassword" placeholder="Konfirmasi Password" name="password_confirmation">
-                        </div>
-
                     </div>
 
                     <div class="card-footer">
-                        <button type="submit" class="btn btn-primary">Simpan</button>
+                        <button type="submit" class="btn btn-primary">Thêm mới</button>
                         <a href="{{route('users.index')}}" class="btn btn-default">
                             Batal
                         </a>
