b592d778fd28a40b9f2e15a1ec2d667cb9b4d47a
Nguyễn Thị Kim Yến <yenn0750@gmail.com>
2023-03-24 16:29:08 +0700

    bug_bug

--- resources/views/admin/products/create.blade.php
+++ resources/views/admin/products/create.blade.php
@@ -37,6 +37,34 @@
                                 <span class="text-danger">{{ $message }}</span>
                             @enderror
                         </div>
+                        <label for="exampleInputName">Ảnh</label>
+                        <div class="input-group hdtuto control-group lst increment">
+                            <div class="list-input-hidden-upload">
+                                <input hidden type="file" name="images[]" id="file_upload"
+                                       class="myfrm form-control hidden">
+                            </div>
+                            <div class="input-group-btn">
+                                <button class="btn btn-success btn-add-image" type="button"><i
+                                        class="fldemo glyphicon glyphicon-plus"></i>+Add image
+                                </button>
+                            </div>
+                        </div>
+                        <div class="list-images">
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputEmail">Nội dung</label>
+                            <textarea class="summernote form-control @error('description') is-invalid @enderror"
+                                      id="text" cols="30" rows="10" placeholder="Mô tả"
+                                      name="description">{{old('description')}}</textarea>
+                            @error('description') <span class="text-danger">{{$message}}</span> @enderror
+                            @include('ckfinder::setup')
+                        </div>
+                        <div class="col-md-12 mb-2" id="none-image">
+                            <img id="preview-image-before-upload"
+                                 src="{{asset('images-UI/notfound.jpg')}}"
+                                 alt="preview image" style="max-height: 250px;">
+                        </div>
+
                         <div class="form-group">
                             <label for="exampleInputEmail">Mã sản phẩm</label>
                             <input type="text" class="form-control @error('name') is-invalid @enderror"
@@ -159,6 +187,70 @@
                         $('#record-'+button_id+'').remove();
                         x--;
                         if (x == 0) $("#table_variant").addClass('d-none');
+                    });
+                });
+            </script>
+        @include('ckfinder::setup')
+        <script src={{ url('ckeditor/ckeditor.js') }}></script>
+            <script>
+                CKEDITOR.replace('text', {
+                    filebrowserBrowseUrl: '{{ route('ckfinder_browser') }}',
+                    filebrowserUploadUrl: '{{ route('ckfinder_connector') }}',
+                    filebrowserWindowWidth: '1000',
+                    filebrowserWindowHeight: '700',
+                    height: '1000'
+                });
+            </script>
+
+
+            <script type="text/javascript">
+                $(document).ready(function () {
+                    $(".btn-add-image").click(function () {
+                        let countImage = $("input[name='images[]']").map(function () {
+                            if ($(this).val() !== '')
+                                return $(this).val();
+                        })
+                        if (countImage.length > 9) {
+                            alert('Không được up quá 10 ảnh!')
+                        } else $('#file_upload').trigger('click');
+
+                    });
+
+                    $('.list-input-hidden-upload').on('change', '#file_upload', function (event) {
+                        let today = new Date();
+                        let time = today.getTime();
+                        let image = event.target.files[0];
+                        let file_name = event.target.files[0].name;
+                        let box_image = $('<div class="box-image"></div>');
+                        box_image.append('<img src="' + URL.createObjectURL(image) + '" class="picture-box">');
+                        box_image.append('<div class="wrap-btn-delete"><span data-id=' + time + ' class="btn-delete-image">x</span></div>');
+                        $(".list-images").append(box_image);
+
+                        $(this).removeAttr('id');
+                        $(this).attr('id', time);
+                        let input_type_file = '<input type="file" hidden name="images[]" id="file_upload" class="myfrm form-control hidden">';
+                        $('.list-input-hidden-upload').append(input_type_file);
+                        let countImage = $("input[name='images[]']").map(function () {
+                            if ($(this).val() !== '')
+                                return $(this).val();
+                        })
+                        if (countImage.length > 0) {
+                            $("#none-image").addClass("d-none");
+                        }
+
+                    });
+
+                    $(".list-images").on('click', '.btn-delete-image', function () {
+                        let id = $(this).data('id');
+                        $('#' + id).remove();
+                        $(this).parents('.box-image').remove();
+                        let countImage = $("input[name='images[]']").map(function () {
+                            if ($(this).val() !== '')
+                                return $(this).val();
+                        })
+                        if (countImage.length == 0) {
+                            $("#none-image").removeClass("d-none");
+                        }
                     });
                 });
             </script>
